ea5682ae46c75b58f436a8b7da1ae88a908ed23c
amirkzm0098 <amirkzm0098@gmail.com>
2023-04-15 23:08:17 +0330

    implemented UI of profile page

---
+++ Profile/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const lightGrayColor = Color(0xFFE9E9F3);
+const darkGrayColor = Color(0xFF454545);
+
+const darkBlueColor = Color(0xFF48A2F5);
+const lightBlueColor = Color(0xFFCCDBEE);
